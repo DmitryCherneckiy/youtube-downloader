0bd2a15f3595b040c137e014b154a0e2b85e2149
Dmitry Cherneckiy <cherneckiyd@gmail.com>
2022-08-22 18:22:43 +0300

    added settings command and mode switches

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from config import BOT_TOKEN, DB_URL
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 db_connection = psycopg2.connect(DB_URL, sslmode="require")
 db_object = db_connection.cursor()
 
@@ -30,6 +32,45 @@ def start_message(message):  # add user_id to database and sending welcome messa
                                       "2️⃣ - Download only video.\n"
                                       "3️⃣ - Download only audio.\n\n"
                                       "If you want to change your mode - use the /settings command.")
+
+
+@bot.message_handler(commands=["settings"])  # bot mode selection
+def call_settings_buttons(message):
+    buttons(message)
+
+
+def buttons(message):
+    user_id = message.from_user.id
+    db_object.execute(f"SELECT mode FROM users WHERE user_id = {user_id}")
+    result = db_object.fetchone()
+    user_mode = result[0]  # user mode check
+
+    keyboard = types.InlineKeyboardMarkup()
+    button_one = types.InlineKeyboardButton(text="Mode 1️⃣", callback_data="mode_one")
+    button_two = types.InlineKeyboardButton(text="Mode 2️⃣", callback_data="mode_two")
+    button_three = types.InlineKeyboardButton(text="Mode 3️⃣", callback_data="mode_three")
+    keyboard.add(button_one, button_two, button_three)
+
+    bot.send_message(message.chat.id, f"Choose mode of bot work.\n"
+                                      f"Your mode now is {user_mode}.", parse_mode="Markdown", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def buttons_handler(call):  # mode switches
+    user_id = call.from_user.id
+    if call.data == "mode_one":
+        db_object.execute(f"UPDATE users SET mode = 1 WHERE user_id = {user_id}")
+        bot.send_message(call.message.chat.id, "Your mode was set to 1️⃣\n"
+                                               "Video and audio download.")
+    elif call.data == "mode_two":
+        db_object.execute(f"UPDATE users SET mode = 2 WHERE user_id = {user_id}")
+        bot.send_message(call.message.chat.id, "Your mode was set to 2️⃣\n"
+                                               "Only video download.")
+    elif call.data == "mode_three":
+        db_object.execute(f"UPDATE users SET mode = 3 WHERE user_id = {user_id}")
+        bot.send_message(call.message.chat.id, "Your mode was set to 3️⃣\n"
+                                               "Only audio download.")
+    db_connection.commit()
 
 
 if __name__ == '__main__':
